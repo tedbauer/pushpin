60c742ebc29276ef086fde54249209735dd672fb
Theodore Bauer <tjb272@cornell.edu>
2024-04-30 02:23:18 +0000

    Clean up code

---
+++ src/gen_site.rs
@@ -0,0 +1,128 @@
+use std::fs;
+use std::fs::File;
+use std::io::Read;
+use std::io::Write;
+
+use crate::Config;
+use anyhow::{anyhow, Result};
+use pulldown_cmark::CowStr;
+use pulldown_cmark::DefaultBrokenLinkCallback;
+use pulldown_cmark::Event;
+use pulldown_cmark::LinkType;
+use pulldown_cmark::Parser;
+use pulldown_cmark::Tag;
+use pulldown_cmark::TagEnd;
+use pulldown_cmark::TextMergeStream;
+use tera::Tera;
+
+fn push_toc(iter: &mut Vec<Event>, config: Config) -> () {
+    for post in config.posts {
+        let post_name = post.name;
+        iter.push(Event::Start(Tag::List(None)));
+        iter.push(Event::Start(Tag::Item));
+        iter.push(Event::Start(Tag::Link {
+            link_type: LinkType::Inline,
+            dest_url: format!("posts/{post_name}.html").into(),
+            title: post.title.clone().into(),
+            id: post_name.into(),
+        }));
+        iter.push(Event::Text(post.title.clone().into()));
+        iter.push(Event::End(TagEnd::Link));
+        iter.push(Event::End(TagEnd::Item));
+        iter.push(Event::End(TagEnd::List(false)));
+    }
+}
+
+fn expand_macros<'a>(
+    iter: TextMergeStream<'a, Parser<'a, DefaultBrokenLinkCallback>>,
+    config: &Config,
+) -> Result<Vec<Event<'a>>> {
+    let mut transformed: Vec<Event> = Vec::new();
+    for event in iter {
+        match event {
+            Event::Text(CowStr::Boxed(ref text))
+                if **text == *<&str as Into<String>>::into("[[ListPosts]]") =>
+            {
+                push_toc(&mut transformed, config.clone())
+            }
+            _ => transformed.push(event.clone()),
+        }
+    }
+    Ok(transformed.clone())
+}
+
+fn generate_page(
+    markdown: &str,
+    template_name: &str,
+    tera: &Tera,
+    config: &Config,
+) -> Result<String> {
+    let iterator = TextMergeStream::new(Parser::new(&markdown));
+    let html_content = expand_macros(iterator, &config).and_then(|events| {
+        let mut html_output = String::new();
+        pulldown_cmark::html::push_html(&mut html_output, events.into_iter());
+        Ok(html_output)
+    })?;
+
+    let mut context = tera::Context::new();
+    context.insert("content", &html_content);
+
+    tera.render(template_name, &context)
+        .map_err(|err| anyhow!("tera render failed: {err}"))
+}
+
+fn write_page(
+    markdown_path: &str,
+    target_path: &str,
+    template_name: &str,
+    tera: &Tera,
+    config: &Config,
+) -> Result<()> {
+    let markdown_file = File::open(format!("pages/{markdown_path}"));
+    let mut markdown_content = String::new();
+    markdown_file?.read_to_string(&mut markdown_content)?;
+
+    let rendered_html = generate_page(&markdown_content, template_name, tera, config)?;
+
+    let mut target_file = File::create(target_path).unwrap();
+    write!(target_file, "{}", rendered_html).map_err(|err| anyhow!("error: {err}"))
+}
+
+fn load_templates() -> Result<Tera> {
+    let mut tera = Tera::new("templates/**/*")?;
+
+    let index_template = include_str!("templates/index.html.template");
+    tera.add_raw_template("index", &index_template)?;
+
+    let post_template = include_str!("templates/post.html.template");
+    tera.add_raw_template("post", &post_template)?;
+
+    Ok(tera)
+}
+
+pub(crate) fn generate(config: &Config) -> Result<usize> {
+    let tera = load_templates()?;
+
+    let stylesheet = include_str!("style/stylesheet.css");
+    fs::create_dir_all("style")?;
+    let mut stylesheet_file = File::create("style/stylesheet.css")?;
+    write!(stylesheet_file, "{}", stylesheet)?;
+
+    write_page(&config.homepage, "index.html", "index", &tera, config)?;
+    let mut num_generated_files = 1;
+
+    fs::create_dir_all("posts")?;
+    for post in &config.posts {
+        let name = &post.name;
+        write_page(
+            &post.path,
+            format!("posts/{name}.html").as_str(),
+            "post",
+            &tera,
+            config,
+        )?;
+
+        num_generated_files += 1;
+    }
+    Ok(num_generated_files)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
-use handlebars::Handlebars;
-use pulldown_cmark::{CowStr, LinkType, Tag, TagEnd, TextMergeStream};
-use pulldown_cmark::{Event, Parser};
-use serde::Serialize;
-use std::collections::HashMap;
-use std::fs;
+use pulldown_cmark::Event;
+use pulldown_cmark::{LinkType, Tag, TagEnd};
+
 use std::fs::File;
 use std::io::Read;
-use std::io::Write;
-use tera::Tera;
-use tinytemplate::TinyTemplate;
+
 use yaml_rust::{Yaml, YamlLoader};
+
+mod gen_site;
 
 #[derive(Debug, Clone)]
 struct Post {
@@ -23,206 +20,6 @@ struct Post {
 struct Config {
     homepage: String,
     posts: Vec<Post>,
-}
-
-#[derive(Serialize)]
-struct Context {
-    content: String,
-}
-
-fn push_toc(iter: &mut Vec<Event>, config: Config) -> () {
-    for post in config.posts {
-        let post_name = post.name;
-        iter.push(Event::Start(Tag::List(None)));
-        iter.push(Event::Start(Tag::Item));
-        iter.push(Event::Start(Tag::Link {
-            link_type: LinkType::Inline,
-            dest_url: format!("posts/{post_name}.html").into(),
-            title: post.title.clone().into(),
-            id: post_name.into(),
-        }));
-        iter.push(Event::Text(post.title.clone().into()));
-        iter.push(Event::End(TagEnd::Link));
-        //iter.push(Event::Text(CowStr::Boxed(post.title.into())));
-        iter.push(Event::End(TagEnd::Item));
-        iter.push(Event::End(TagEnd::List(false)));
-    }
-}
-
-fn main() -> Result<(), std::io::Error> {
-    let stylesheet = r#"
-
-    .header {
-        background-color: dodgerblue;
-    }
-
-    "#;
-
-    let mut file = File::open("PUSHPIN.yaml")?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-
-    let mut yaml_loader = YamlLoader::load_from_str(&contents).unwrap();
-    let yaml_doc = &yaml_loader[0];
-    let config = parse_config(&yaml_doc);
-
-    let home_filename = &config.homepage;
-    let home_path = format!("pages/{home_filename}");
-    let mut home_file = File::open(home_path);
-    let mut home_contents = String::new();
-    home_file?.read_to_string(&mut home_contents)?;
-
-    let iterator = TextMergeStream::new(Parser::new(&home_contents));
-    let mut transformed: Vec<Event> = Vec::new();
-    for event in iterator {
-        match event {
-            Event::Text(CowStr::Boxed(ref text))
-                if **text == *<&str as Into<String>>::into("[[ListPosts]]") =>
-            {
-                push_toc(&mut transformed, config.clone())
-            }
-            _ => transformed.push(event),
-        }
-    }
-
-    let mut html_output = String::new();
-    pulldown_cmark::html::push_html(&mut html_output, transformed.into_iter());
-
-    let template_str = r#"
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>My Markdown Content</title>
-        <link rel="stylesheet" href="style/stylesheet.css">
-        <link
-        href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap"
-        rel="stylesheet">
-        <style>
-
-        .container {
-            width: 400px;
-            margin: 20px;
-            padding: 25px;
-            border-radius: 20px;
-            border-width: 1px;
-            border-color: black;
-            border-style: solid;
-            box-shadow: black 5px 5px;
-
-            font-family: 'Merriweather', serif;
-            position: relative;
-        }
-
-        .outer {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            padding: 50px;
-            position: relative;
-
-        }
-
-        </style>
-    </head>
-    <body>
-        <div class="outer">
-        <div class="container">
-        {{content}}
-        </div>
-        </div>
-    </body>
-    </html>
-"#;
-
-    let mut tera = Tera::new("templates/**/*").unwrap();
-    tera.add_raw_template("hello", &template_str).unwrap();
-
-    let mut context = tera::Context::new();
-    context.insert("content", &html_output);
-
-    let rendered = tera.render("hello", &context).unwrap();
-
-    let mut index = File::create("index.html").unwrap();
-    write!(index, "{}", rendered);
-
-    fs::create_dir("style");
-    let mut style = File::create("style/stylesheet.css").unwrap();
-    write!(style, "{}", stylesheet);
-
-    fs::create_dir("posts");
-    for post in config.posts {
-        let post_filename = &post.path;
-        let post_path = format!("pages/{post_filename}");
-        let mut post_file = File::open(post_path);
-        let mut post_contents = String::new();
-        post_file?.read_to_string(&mut post_contents)?;
-
-        let iterator = TextMergeStream::new(Parser::new(&post_contents));
-        let mut html_output = String::new();
-        pulldown_cmark::html::push_html(&mut html_output, iterator);
-
-        let template_str = r#"
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>My Markdown Content</title>
-        <link rel="stylesheet" href="style/stylesheet.css">
-        <link
-        href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap"
-        rel="stylesheet">
-        <style>
-
-        .container {
-            width: 400px;
-            margin: 20px;
-            padding: 25px;
-            border-radius: 20px;
-            border-width: 1px;
-            border-color: black;
-            border-style: solid;
-            box-shadow: black 5px 5px;
-
-            font-family: 'Merriweather', serif;
-            position: relative;
-        }
-
-        .outer {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            padding: 50px;
-            position: relative;
-
-        }
-
-        </style>
-    </head>
-    <body>
-        <div class="outer">
-        <div class="container">
-        Back
-        {{content}}
-        </div>
-        </div>
-    </body>
-    </html>
-"#;
-
-        let mut tera = Tera::new("templates/**/*").unwrap();
-        tera.add_raw_template("hello2", &template_str).unwrap();
-
-        let mut context = tera::Context::new();
-        context.insert("content", &html_output);
-
-        let rendered = tera.render("hello2", &context).unwrap();
-
-        let post_name = post.name;
-        let mut post = File::create(format!("posts/{post_name}.html")).unwrap();
-        write!(post, "{}", rendered);
-    }
-    println!("done.");
-
-    Ok(())
 }
 
 fn parse_config(yaml_doc: &Yaml) -> Config {
@@ -242,4 +39,21 @@ fn parse_config(yaml_doc: &Yaml) -> Config {
         });
     }
     Config { homepage, posts }
+}
+
+fn main() -> Result<(), std::io::Error> {
+    let mut config_file = File::open("PUSHPIN.yaml")?;
+    let mut contents = String::new();
+    config_file.read_to_string(&mut contents)?;
+
+    let yaml_loader = YamlLoader::load_from_str(&contents).unwrap();
+    let yaml_doc = &yaml_loader[0];
+    let config = parse_config(&yaml_doc);
+
+    match gen_site::generate(&config) {
+        Ok(num_files_generated) => println!("Success: generated {num_files_generated} files."),
+        Err(err) => println!("Error: {err}"),
+    };
+
+    Ok(())
 }
